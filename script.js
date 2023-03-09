85b7a5e270b8cd78b3ad7a2652fedcd889bb6b1e
amancior23 <126115676+amancior23@users.noreply.github.com>
2023-03-09 13:13:22 -0800

    Platform
    we added drop down menus to the platform

---
+++ script.js
@@ -0,0 +1,36 @@
+function fun(){
+    let x = document.getElementById("collapse");
+    if(x.style.display === "none"){
+        x.style.display = "block"
+    }
+    else
+    {
+        x.style.display = "none";
+    }
+    
+}
+function fun2(){
+    let x = document.getElementById("collapse2");
+    if(x.style.display === "block"){
+        x.style.display = "none"
+    }
+    else
+    {
+        x.style.display = "block";
+    }
+    
+}
+function fun3(){
+    let x = document.getElementById("collapse3");
+    if(x.style.display === "block"){
+        x.style.display = "none"
+    }
+    else
+    {
+        x.style.display = "block";
+    }
+    
+}
+
+
+
